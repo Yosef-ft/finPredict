0314be522f081526313e5b3310bf488c444ac654
Yosef-ft <yosefetene02@gmail.com>
2024-07-17 11:31:10 +0300

    writing to Hdf5

--- Predict high and low/Data/database.py
+++ Predict high and low/Data/database.py
@@ -1,8 +1,12 @@
 from typing import *
+import logging
 
 import pandas as pd
 import numpy as np 
 import h5py
+
+
+logger = logging.getLogger()
 
 class Hdf5Client:
     def __init__(self):
@@ -14,5 +18,44 @@ class Hdf5Client:
         if symbol not in self.hdf.keys():
             self.hdf.create_dataset(symbol, shape= (None, 6), dtype='float64')
             self.flush()
+    
+    def write_data(self, symbol: str, data: List[Tuple]):
+
+        min_ts, max_ts = self.get_first_last_timestamp(symbol)
+
+        if min_ts is None:
+            min_ts = float('inf')
+            max_ts = 0
+
+        filtered_data = []
+
+        for candle in data:
+            if candle[0] < min_ts:
+                filtered_data.append(candle)
+            elif candle[0] > max_ts:
+                filtered_data.append(candle)
+
+        if len(filtered_data) == 0:
+            logger.info('No Additioal data exist of %s', symbol)
+            return 
+
+        data_array = np.array(data)
+
+        self.hdf[symbol].resize(self.hdf[symbol].shape[0] + data_array.shape[0])
+        self.hdf[symbol][-data_array.shape[0]:] = data_array
+
+        self.hdf.flush()
+
+    def get_first_last_timestamp(self, symbol: str) -> Union[Tuple[None, None], Tuple[float, float]]:
+
+        existing_data = self.hdf[symbol][:]
+
+        if len(existing_data) == 0:
+            return None, None
+        
+        first_ts = min(existing_data, key= lambda x: x[0])[0]
+        last_ts = max(existing_data, key= lambda x: x[0])[0]
+
+        return first_ts, last_ts
     
     
